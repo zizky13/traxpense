0249a567f4d23d7e25e5ce9570e6f63872f51825
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-05-28 09:12:38 +0700

    Refactor UI for expenses/income record, add delete record functionality

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -2,21 +2,8 @@ import { Text, View, StyleSheet, Dimensions } from "react-native";
 import { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SummaryCard from "../components/SummaryCard";
-// import { vertexAI } from "../firebase";
-// import { getGenerativeModel } from "firebase/vertexai-preview";
 
 export default ReportScreen = () => {
-
-  // const model = getGenerativeModel(vertexAI, {
-  //   model: "gemini-1.5-pro-preview-0409",
-  // });
-  // const prompt = "Tell me signature dish of Indonesia";
-
-  // useEffect(() => {
-  //   model.generateContent(prompt).then((result) => {
-  //     setData(result.response.text);
-  //   });
-  // }, []);
 
   return (
     <SafeAreaView style={{flex: 1}}>
